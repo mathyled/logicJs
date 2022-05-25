9bafe22edf1952439bb4eb845babf0330315938e
unknown <mathiasimagine@gmail.com>
2022-05-25 18:51:23 -0300

    add interview from HackRank

---
+++ interviewFour.js
@@ -0,0 +1,17 @@
+//Dada una matriz de enteros, calcule las proporciones de sus elementos que son positivos , negativos y cero .
+// Imprime el valor decimal de cada fracción en una nueva línea con 6 lugares después del decimal.
+
+
+let arr = [1, 1, 0, -1, -1]
+// cantidad              proporciones
+// 1 -> 2                 2/5
+// 0 -> 1                 1/5
+// -1 -> 2                2/5  
+ 
+
+//Imprime las proporciones de valores positivos, negativos y cero en la matriz. Cada valor debe imprimirse en una línea separada condígitos después del decimal. La función no debe devolver un valor.
+
+function plusMinus(arr) {
+    // Write your code here
+
+  }
